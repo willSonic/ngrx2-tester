4b033617fff2fabc1bef55d74f195ea5709fb7e1
Willie Streeter <willie.l.streeter@kp.org>
2016-05-30 22:01:46 -0700

    updaate initial commit

---
+++ src/app.ts
@@ -0,0 +1,96 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app',
+  template: `
+    <nav>
+      <a linkTo="/">Collection</a>
+      <a linkTo="/book/find">Find Book</a>
+    </nav>
+    <route-view></route-view>
+  `
+})
+export default class App { }
+
+
+/*
+
+import { Component, ChangeDetectionStrategy} from '@angular/core';
+import { ArtistList} from "./components/artist-list";
+import { AudioList} from "./components/audio-list";
+import { getArtists } from "./actions/artistsAction";
+import { playAudioItem } from "./actions/audioplayerAction";
+import { AudioServiceAction } from "./actions/audioServiceAction";
+import { artistAsArraySelector } from "./selectors/artist.selector";
+import { constructedPlaylistItem, playlistArraySelector } from "./selectors/playlist.selector";
+import {AsyncPipe } from "@angular/common";
+import { Subject } from 'rxjs';
+import {Store, Action} from "@ngrx/store";
+
+@Component({
+    selector: `artist-playlist-app`,
+    template: `
+	<div id="layout" class="pure-g">
+		<div class="sidebar pure-u-1 pure-u-md-1-4">
+			<div class="header">
+				<h1 class="brand-title">Audio Artist NGRX Store</h1>
+				<h2 class="brand-tagline">Wiki Audio</h2>
+			</div>
+		</div>
+		<div class="content pure-u-1">
+		   <div class="pure-g">
+		      <div class="pure-u-1-3">
+                    <artist-list
+                        [artistList]="(artistList | async)"
+                        (createPlaylistItem)="audioServiceActions.createPlaylistItem($event)" 
+                        >
+                    </artist-list>
+              </div>
+		      <div class=" pure-u-1-3 custom-restricted-width">
+                    <audio-list
+                        [audioList]="(audioList | async)"
+                        (playAudioItem)="actions$.next(playArtistAction($event))"
+                        >
+                    </audio-list>
+              </div>
+		    </div>
+		</div>
+	</div>
+	`,
+    directives: [ArtistList, AudioList],
+    pipes: [AsyncPipe],
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ArtistPlaylistApp{
+
+    artistList: any;
+    audioList: any;
+    playlist:any;
+    actions$ = new Subject<Action>();
+    playArtistAction = playAudioItem;
+
+    private subscription;
+
+
+    constructor(public store: Store<any>,  private audioServiceActions:AudioServiceAction) {
+        this.artistList   = store.let(artistAsArraySelector);
+        /*this.playlist     = store.let(playlistArraySelector).subscribe((val) =>{
+                                                                     console.log('[ArtistPlaylistApp] playlist  change',val)
+                                                                   });*/
+        this.audioList    = store.let(constructedPlaylistItem);
+        /*this.subscription  = store.select('playlist').subscribe((val) =>{
+                                                                     console.log('[ArtistPlaylistApp] subscription  change',val)
+                                                                   });*/
+
+        this.actions$.subscribe(store);
+        this.actions$.next(getArtists());
+
+    }
+
+    ngOnDestroy() {
+        this.store.unsubscribe();
+    }
+
+}
+ */
---
+++ src/main.browser.ts
@@ -0,0 +1,98 @@
+import { bootstrap } from '@angular/platform-browser-dynamic';
+import { provideStore } from '@ngrx/store';
+import {provideStore, usePreMiddleware, usePostMiddleware, Middleware} from "@ngrx/store";
+import {APP_REDUCERS} from "./reducers/reducers";
+import { runEffects } from '@ngrx/effects';
+
+import App from './app'
+import reducer from './reducers';
+import effects from './effects';
+import services from './services';
+import actions from './actions';
+/*import {bootstrap} from '@angular/platform-browser-dynamic';
+import {provide} from '@angular/core';
+import {HTTP_PROVIDERS, BrowserXhr} from '@angular/http';
+import {ELEMENT_PROBE_PROVIDERS} from '@angular/platform-browser/index';
+import {ArtistPlaylistApp} from './artistPlaylist-app';
+import {AudioServiceAction} from './actions/audioServiceAction';
+import {provideStore, usePreMiddleware, usePostMiddleware, Middleware} from "@ngrx/store";
+import {APP_REDUCERS} from "./reducers/reducers";
+import {APP_SAGAS} from "./sagas/sagas";
+import audiomachineSagas from "./sagas/audiomachine";
+import {installSagaMiddleware} from 'store-saga';
+
+
+import { apiInjectables } from '../api/api.injectable.ts';
+
+
+import {CustomBrowserXhr} from '../util/custom.xhr';
+
+const actionLog : Middleware = action => {
+    return action.do(val => {
+        console.warn('DISPATCHED ACTION: ', val)
+    });
+};
+
+const stateLog : Middleware = state => {
+    return state.do(val => {
+        console.info('NEW STATE: ', val)
+    });
+};
+
+
+export function main() {
+  return bootstrap(ArtistPlaylistApp, [
+      apiInjectables,
+      ELEMENT_PROBE_PROVIDERS,
+      HTTP_PROVIDERS,
+      AudioServiceAction,
+      provide(BrowserXhr, { useClass: CustomBrowserXhr }),
+      provideStore(APP_REDUCERS),
+      usePreMiddleware(actionLog),
+      usePostMiddleware(stateLog),
+      installSagaMiddleware(...audiomachineSagas)
+  ])
+  .catch(err => console.error(err));
+}
+
+document.addEventListener('DOMContentLoaded', main);
+*/
+bootstrap(App, [
+  /**
+   * provideStore is run once at application bootstrap, accepting a reducer
+   * function or object map of reducer functions. If passed an object of
+   * reducers, combineReducers will be run creating your application
+   * meta-reducer. This returns all providers for an @ngrx/store
+   * based application.
+   *
+   * Source: https://github.com/ngrx/store/blob/master/src/ng2.ts#L43-L69
+   */
+  provideStore(reducer),
+
+  /**
+   * runEffects configures all providers for @ngrx/effects. Observables decorated
+   * as an @Effect() within the supplied services will ultimately be merged,
+   * with output of relevant (registered as effects) actions being
+   * dispatched into your application store. Any side-effects in
+   * your application should be registered as effects.
+   *
+   * Source: https://github.com/ngrx/effects/blob/master/lib/run-effects.ts#L8-L20
+   */
+  runEffects(effects),
+
+
+  /**
+   * connectRouterToStore configures additional providers that synchronize
+   * router state with @ngrx/store. This lets you debug router state using
+   * ngrx/store and to change the location by dispatching actions.
+   */
+  connectRouterToStore(),
+
+
+  /**
+   * Finall we provide additional services and action creators so they can
+   * be used by all of our components, effects, and guards.
+   */
+  services,
+  actions
+]);
